e042f3f084d13c2fde75d63502274209a1e333fd
root <danko.jakovina@gmail.com>
2018-10-28 21:54:12 +0100

    Login metoda prebačena na model usera

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const bcrypt   = require('bcrypt-nodejs');
+const jwt      = require('jsonwebtoken');
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -7,8 +10,46 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     username: DataTypes.STRING
   }, {});
+  User.login = (req) => {
+    return new Promise((resolve, reject) => {
+      let password = req.body.password; 
+      let username = req.body.username;
+      User.findOne({where: {
+        username: username
+    }}).then(user => {
+        console.log(user)
+        if(user) {
+            bcrypt.compare(password, user.password, function(err, result) {
+                console.log(result, 'passw');
+                //ako je password valjan generirati token i vrtatiti korisniku podatke
+                if(result) {
+                    jwt.sign(JSON.stringify(user.id), 'secret', (err, token) => {
+                        const userObj = user.dataValues;
+                        userObj.token = token;
+                        resolve(userObj);
+                    });
+                    
+                } else {
+                    reject();
+                }
+                
+                // res == true
+            
+              
+        })  
+        }
+    })
+    
+    .catch(error => {
+    // error;    
+    });
+    })
+  
+  }
   User.associate = function(models) {
     // associations can be defined here
   };
   return User;
 };
+
+
--- routes/login-route.js
+++ routes/login-route.js
@@ -8,37 +8,10 @@ router.post('/login', (req, res) => {
     //console.log(req.body) 
     let password = req.body.password; 
     let username = req.body.username;
-        User.findOne({where: {
-            username: username
-        }}).then(user => {
-            console.log(user)
-            if(user) {
-                bcrypt.compare(password, user.password, function(err, result) {
-                    console.log(result, 'passw');
-                    //ako je password valjan generirati token i vrtatiti korisniku podatke
-                    if(result) {
-                        jwt.sign(JSON.stringify(user.id), 'secret', (err, token) => {
-                            const userObj = user.dataValues;
-                            userObj.token = token;
-                            res.send(userObj);
-                        });
-                        
-                    } else {
-                        res.send('lozinka nije valjana');
-                    }
-                    
-                    // res == true
-                
-                  
-            })  
-            }
-        })
-     
-    .catch(error => {
-        // error;    
-    }); 
-    
-    
-})
+    console.log('logion')
+    User.login(req).then((user) => {
+        res.send(user)
+    });
+});
 
 module.exports = router;
